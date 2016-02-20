b2fbe27628c8efe8df97096b8e8c418c939073b8
Marvin Arnold <marvinmarnold@gmail.com>
2016-02-20 14:46:22 -0600

    hello world

--- src/android/org/stringraymappingproject/cordova/Telephony.java
+++ src/android/org/stringraymappingproject/cordova/Telephony.java
@@ -1,13 +1,22 @@
-package com.megster.cordova;
+package org.stingraymappingproject.cordova;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.json.JSONArray;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaArgs;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.app.Activity;
 import android.content.Context;
-//import android.telephony.TelephonyManager;
-import java.util.Set;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.Manifest;
+
 
 /**
  * PhoneGap Plugin for accessing Telephony information
@@ -24,15 +33,21 @@ public class Telephony extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+      Context context = this.cordova.getActivity().getApplicationContext();
 
-        LOG.d(TAG, "action = " + action);
+//        LOG.d(TAG, "action = " + action);
 
-        if (telephonyManager == null) {
-            telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        }
+      if (telephonyManager == null) {
+          telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+      }
 
-        if (action.equals(GET_CID)) {
-            return telephonyManager.getDeviceId();
-        }
+      if (action.equals(GET_CID)) {
+          JSONObject result = new JSONObject();
+          result.put("sting", 923);
+
+          callbackContext.success(result);
+      }
+
+      return true;
     }
 }
