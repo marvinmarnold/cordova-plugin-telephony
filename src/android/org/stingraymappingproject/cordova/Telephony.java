bb1799bcdb3bfa2f19964563fcbb90a7fc6d859c
Marvin Arnold <marvinmarnold@gmail.com>
2016-02-20 19:33:26 -0600

    read CID, LAC, PSC

--- src/android/org/stingraymappingproject/cordova/Telephony.java
+++ src/android/org/stingraymappingproject/cordova/Telephony.java
@@ -4,6 +4,13 @@ package org.stingraymappingproject.cordova;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
 
 public class Telephony extends CordovaPlugin {
 
@@ -11,9 +18,25 @@ public class Telephony extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("getCid")) {
+            Context context = this.cordova.getActivity().getApplicationContext();
+            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            int phoneType = tm.getPhoneType();
+            JSONObject result = new JSONObject();
+            String phoneTypeName = "unknown";
 
-            String message = "452";
-            callbackContext.success(message);
+            switch(phoneType) {
+                case TelephonyManager.PHONE_TYPE_NONE:
+                case TelephonyManager.PHONE_TYPE_SIP:
+                case TelephonyManager.PHONE_TYPE_GSM:
+                    result = buildGSMResult(tm);
+                    phoneTypeName = "gsm";
+                    break;
+                case TelephonyManager.PHONE_TYPE_CDMA:
+                    phoneTypeName = "cdma";
+                    break;
+            }
+            result.put("phoneType", phoneTypeName);
+            callbackContext.success(result);
 
             return true;
 
@@ -23,4 +46,31 @@ public class Telephony extends CordovaPlugin {
 
         }
     }
+
+    private JSONObject buildGSMResult(TelephonyManager tm) {
+        JSONObject result = new JSONObject();
+        String mncMCC = tm.getNetworkOperator();
+
+        if (mncMCC != null && mncMCC.length() >= 3 ) {
+            try {
+//                GsmCellLocation gsmCellLocation = (GsmCellLocation) tm.getCellLocation();
+                result.put("mnc", Integer.parseInt(mncMCC.substring(3)));
+                result.put("mcc", Integer.parseInt(mncMCC.substring(0, 3)));
+
+                GsmCellLocation gsmCellLocation = (GsmCellLocation) tm.getCellLocation();
+                if (gsmCellLocation != null) {
+                    result.put("cid", gsmCellLocation.getCid());
+                    result.put("lac", gsmCellLocation.getLac());
+                    result.put("psc", gsmCellLocation.getPsc());
+                }
+            }catch (Exception e) {
+            }
+
+        }
+
+
+        return result;
+    }
+
+
 }
