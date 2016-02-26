56471649bd3579e0d98f77ffcc0bd25f5ec78753
Marvin Arnold <marvinmarnold@gmail.com>
2016-02-25 22:34:30 -0600

    change to window.plugins.telephony

--- www/telephony.js
+++ www/telephony.js
@@ -1,8 +1,18 @@
 /*global cordova*/
-module.exports = {
+var telephony = {
 
     refresh: function (success, failure) {
       cordova.exec(success, failure, "Telephony", "refresh", []);
     },
 
 }
+
+
+cordova.addConstructor(function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.telephony = telephony;
+  return window.plugins.telephony;
+});
